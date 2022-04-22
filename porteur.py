9b1150ae7019825b27e10b16d237a28e91f124a8
Makoorr <azizbenrejeb@gmail.com>
2022-04-22 03:14:29 +0100

    Added combobox for ports

--- porteur.py
+++ porteur.py
@@ -1,33 +1,60 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import Tk
+from tkinter import Tk, ttk
 from tkinter.messagebox import showinfo
+
 import serial.tools.list_ports
 
-def portinput():
-        global inp
-        global test
-        global port
-        global root
+def ports():
+    L=[]
 
-        ports_ori=serial.tools.list_ports.comports()
-        inp=port.get()
+    ports_ori=serial.tools.list_ports.comports()
+    # for testing:
+    # ports_ori=[["COM1","Arduino Port (COM1)","[ACPI\PNP0501\1]"],["COM7", "MediaTek USB Port (COM7)", "[USB VID:PID=0E8D:0003 SER=6 LOCATION=1-2.1]"]]
 
-        if (inp!=None):
-            for port_i, desc, hwid in sorted(ports_ori):
-                txt="{}".format(port_i, desc)
-                print("txt: ",txt)
-                print("inp: ",inp)
-                if(inp==txt):
-                    print("JAWEK BEHI IMED EZEZ EZHAHAHAHA")
-                    test=True
-                    showinfo("Done","Arudino's connected on port "+inp)
-                else:
-                    showinfo("Error","Arduino's port name not found.")
-                if(test==True):
-                    root.destroy()
-                    print("SIUUU")
-                    return(inp)
+    for port_i, desc, hwid in sorted(ports_ori):
+        name="{}: {} [{}]".format(port_i, desc, hwid)
+        txt="{}".format(port_i, desc)
+        print("txt: ",txt)
+        print("name: ",name)
+        L.append(name)
+
+    return (L)
+
+def btncmd():
+    global root
+    global test
+    global port
+    global inp
+
+    inp=port.get()
+
+    ports_ori=serial.tools.list_ports.comports()
+    # for testing:
+    # ports_ori=[["COM1","Arduino Port (COM1)","[ACPI\PNP0501\1]"],["COM7", "MediaTek USB Port (COM7)", "[USB VID:PID=0E8D:0003 SER=6 LOCATION=1-2.1]"]]
+    for port_i, desc, hwid in sorted(ports_ori):
+        name="{}: {} [{}]".format(port_i, desc, hwid)
+        p="{}".format(port_i)
+        txt="{}".format(desc)
+
+        if (inp==name):
+            if("Arduino" in txt):
+                inp=p
+                test=True
+            else:
+                print(txt)
+            
+    if (test):
+        showinfo(
+            title='Done',
+            message="Arudino's connected on port "+inp
+        )
+        root.destroy()
+        return (inp)
+    else:
+        showinfo(
+            title="Error",
+            message="Please choose a correct port"
+        )
 
 def fn():
     global inp
@@ -44,18 +71,19 @@ def fn():
 
     frm = ttk.Frame(root, borderwidth=150)
     frm.grid()
-    ttk.Label(frm, text="Type the arduino's port name (like :  COM11)").grid(column=0, row=0)
-    ttk.Label(frm).grid(column=0,row=1)
 
-    ttk.Entry(frm,textvariable=port).grid(column=0, row=2)
+    ttk.Label(frm, text="Select the Arduino's port").grid(column=0, row=0)
 
-    ttk.Label(frm).grid(column=1, row=3)
+    ttk.Label(frm).grid(column=0,row=1) #Separator
 
-    ttk.Button(frm, text="Select",command=portinput).grid(column=0, row=4)
-    # ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=4)
+    cb=ttk.Combobox(frm,textvariable=port,values=ports(),state="readonly",width=60).grid(column=0, row=2)
+
+    ttk.Label(frm).grid(column=1, row=3) #Separator
+
+    ttk.Button(frm, text="Select",command=btncmd).grid(column=0, row=4)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=5)
     
     root.mainloop()
 
 if __name__=="__main__":
     fn()
-    
